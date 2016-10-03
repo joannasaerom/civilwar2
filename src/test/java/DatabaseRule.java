195479194711aebb7246589df5955e6e8036f30e
Jeremy Fryd <fryd21@gmail.com>
2016-10-03 14:12:34 -0700

    build file structure

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,21 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/ww3", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteUsersQuery = "DELETE FROM users *;";
+      String deleteBasesQuery = "DELETE FROM bases *;";
+      con.createQuery(deleteUsersQuery).executeUpdate();
+      con.createQuery(deleteSightingsQuery).executeUpdate();
+    }
+  }
+
+}
