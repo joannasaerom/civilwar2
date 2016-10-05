425eda49398ba48325e15b8e7ad616410843d4a9
Jeremy Fryd <fryd21@gmail.com>
2016-10-05 16:08:24 -0700

    refactor java files

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -7,18 +7,11 @@ public class Base{
 
   private String location;
   private int playerNum;
-  // private int length;
   private boolean destroyed;
-  // private List<Boolean> nodes = new ArrayList<Boolean>();
-
-  //eventually i think we can put in a second argument for length, I've tried to design it so that later it would be an easy update.
   public Base(String _location, int _playerNum){
     location = _location;
     playerNum = _playerNum;
     destroyed = false;
-    // for (int i = 0; i < length; i++) {
-    //   nodes.add(false);
-    // }
   }
 
   public String getLocation(){
@@ -32,18 +25,5 @@ public class Base{
   public boolean isDestroyed(){
     return destroyed;
   }
-
-  //this method will implement the hit on the correct node and will also return a boolean regarding whether the base has been destroyed or not.
-  // public void hitNode(int _nodeHit){
-  //   nodes.set((_nodeHit-1), true);
-  //   if(nodes.contains(false)){
-  //     destroyed = false;
-  //   } else{
-  //     destroyed = true;
-  //   }
-  // }
-
-
-
 
 }
