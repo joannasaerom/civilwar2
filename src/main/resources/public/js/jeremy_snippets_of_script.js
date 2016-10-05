fedb50da4335fa4f7ffbd4cbabccba984406326f
Jeremy Fryd <fryd21@gmail.com>
2016-10-05 13:38:41 -0700

    plug previous target logs for javascript to pick up on

---
+++ src/main/resources/public/js/jeremy_snippets_of_script.js
@@ -0,0 +1,5 @@
+
+//we need #map to correspond to google maps window
+$("#map").click(function(){
+  $("#attack-result").text("<input name='target' class='hide' value='" + target + "'>");
+});
--- src/main/resources/public/js/scripts.js
+++
@@ -1,7 +0,0 @@
-
-$("#map").click(function(){
-  $("#attack-result").text("<input name='playerHit' class='hide' value='" + playerHit + "'>" +
-    "<input name='baseHit' class='hide' value='" + baseHit + "'>" +
-    "<input name='nodeHit' class='hide' value='" + nodeHit + "'>"
-  );
-});
