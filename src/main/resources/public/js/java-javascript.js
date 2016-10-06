ae1f8b8000f2c9c78f39204b37c224c914f22139
Jeremy Fryd <fryd21@gmail.com>
2016-10-06 13:30:34 -0700

    selection stage debugging, select button disabling

--- src/main/resources/public/js/java-javascript.js
+++ src/main/resources/public/js/java-javascript.js
@@ -4,27 +4,35 @@ var selectedStatesString;
 $(document).ready(function() {
 
   $("#player1-add-bases-button").click(function(){
+    selectedStateNames=[];
+    selectedStatesString = "";
     if(selectedStateNames.length === 0) {
       selectedStates.forEach(function(state){
         selectedStateNames.push(state.name);
       });
       selectedStatesString = selectedStateNames.join(",");
       console.log(selectedStatesString);
-      $("#player1-put-bases-for-java").append("<input name='player1-bases' class='hide' value='" + selectedStatesString + "'>");
+      $("#player1-put-bases-for-java").html("<input name='player1-bases' class='hide' value='" + selectedStatesString + "'>");
     }
-    console.log(selectedStatesString);
+    if (selectedStatesString.length>0) {
+      $("#player1-button").removeClass("disable-button");
+    }
   });
 
   $("#player2-add-bases-button").click(function(){
+    selectedStateNames=[];
+    selectedStatesString = "";
     if(selectedStateNames.length === 0) {
       selectedStates.forEach(function(state){
         selectedStateNames.push(state.name);
       });
       selectedStatesString = selectedStateNames.join(",");
       console.log(selectedStatesString);
-      $("#player2-put-bases-for-java").append("<input name='player2-bases' class='hide' value='" + selectedStatesString + "'>");
+      $("#player2-put-bases-for-java").html("<input name='player2-bases' class='hide' value='" + selectedStatesString + "'>");
     }
-    console.log(selectedStatesString);
+    if (selectedStatesString.length>0) {
+      $("#player2-button").removeClass("disable-button");
+    }
   });
 
 });
